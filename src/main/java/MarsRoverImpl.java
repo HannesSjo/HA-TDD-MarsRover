d004ea273150b374cc37f2e3553ddeea7dc5bce3
Hannes <hannes.sjo2@gmail.com>
2024-05-04 20:15:07 +0300

    Done

--- src/main/java/MarsRoverImpl.java
+++ src/main/java/MarsRoverImpl.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarsRoverImpl implements MarsRover {
 
     private int x;
@@ -12,7 +15,95 @@ public class MarsRoverImpl implements MarsRover {
 
     @Override
     public void move(String commands){
-        //TODO implement
+        if (commands == null || commands.isBlank()) return;
+
+        List<Character> validCommands = new ArrayList<>();
+        validCommands.add('F');
+        validCommands.add('B');
+        validCommands.add('L');
+        validCommands.add('R');
+
+        char[] charCommands = commands.toCharArray();
+
+        for (char command : charCommands){
+            if (!validCommands.contains(command)) {
+                System.out.println("WARN: Unknown command was used and ignored: " + command);
+                continue;
+            }
+
+            switch (command){
+                case 'F':
+                    moveForward();
+                    break;
+                case 'B':
+                    moveBack();
+                    break;
+                case 'L':
+                    rotateLeft();
+                    break;
+                case 'R':
+                    rotateRight();
+                    break;
+            }
+        }
+    }
+
+    private void rotateLeft() {
+        switch (direction) {
+            case NORTH -> direction = Direction.WEST;
+            case WEST -> direction = Direction.SOUTH;
+            case SOUTH -> direction = Direction.EAST;
+            case EAST -> direction = Direction.NORTH;
+        }
+    }
+
+    private void rotateRight() {
+        switch (direction) {
+            case NORTH -> direction = Direction.EAST;
+            case EAST -> direction = Direction.SOUTH;
+            case SOUTH -> direction = Direction.WEST;
+            case WEST -> direction = Direction.NORTH;
+        }
+    }
+
+    private void moveForward() {
+        switch (direction) {
+            case NORTH -> moveY(1);
+            case SOUTH -> moveY(-1);
+            case EAST -> moveX(1);
+            case WEST -> moveX(-1);
+        }
+    }
+
+    private void moveBack() {
+        switch (direction) {
+            case NORTH -> moveY(-1);
+            case SOUTH -> moveY(1);
+            case EAST -> moveX(-1);
+            case WEST -> moveX(1);
+        }
+    }
+
+    private void moveY(int amount) {
+        int maxY = 100;
+        int minY = -100;
+
+        y += amount;
+        if (y > maxY)
+            y = minY + (y - (maxY + 1));
+        else if (y < minY)
+            y = maxY - ((minY - 1) - y);
+    }
+
+    private void moveX(int amount) {
+        int maxX = 100;
+        int minX = -100;
+
+        x += amount;
+        if (x > maxX)
+            x = minX + (x - (maxX + 1));
+        else if (x < minX)
+            x = maxX - ((minX - 1) - x);
     }
 
     @Override
--- src/test/java/MarsRoverTests.java
+++ src/test/java/MarsRoverTests.java
@@ -10,9 +10,66 @@ public class MarsRoverTests {
         MarsRover rover = new MarsRoverImpl(new Coordinates2D(3, 3), Direction.WEST);
 
         //Act
-        rover.move("f");
+        rover.move("F");
 
         //Assert
         Assertions.assertEquals(new Coordinates2D(2, 3), rover.getCurrentLocation());
+    }
+
+    @Test
+    public void testNullInsert() {
+        MarsRover rover = new MarsRoverImpl(new Coordinates2D(3, 3), Direction.WEST);
+        rover.move(null);
+        Assertions.assertEquals(new Coordinates2D(3, 3), rover.getCurrentLocation());
+    }
+
+
+    @Test
+    public void testMoveBackwardSouth() {
+
+        //Arrange (see README for reference to Arrange-Act-Assert Pattern)
+        MarsRover rover = new MarsRoverImpl(new Coordinates2D(70, 70), Direction.SOUTH);
+
+        //Act
+        rover.move("B");
+
+        //Assert
+        Assertions.assertEquals(new Coordinates2D(70, 71), rover.getCurrentLocation());
+    }
+
+    @Test
+    public void testRotation() {
+        MarsRover rover = new MarsRoverImpl(new Coordinates2D(0, 0), Direction.NORTH);
+        rover.move("F");
+        Assertions.assertEquals(new Coordinates2D(0, 1), rover.getCurrentLocation());
+        rover.move("R");
+        rover.move("F");
+        Assertions.assertEquals(new Coordinates2D(1, 1), rover.getCurrentLocation());
+        rover.move("L");
+        rover.move("F");
+        Assertions.assertEquals(new Coordinates2D(1, 2), rover.getCurrentLocation());
+    }
+
+    @Test
+    public void testMultipleInputs(){
+        MarsRover rover = new MarsRoverImpl(new Coordinates2D(0, 0), Direction.NORTH);
+        rover.move("FFRBLB");
+        Assertions.assertEquals(new Coordinates2D(-1, 1), rover.getCurrentLocation());
+    }
+
+    @Test
+    public void testWrapping(){
+        MarsRover rover = new MarsRoverImpl(new Coordinates2D(100, 100), Direction.NORTH);
+        rover.move("F");
+        Assertions.assertEquals(new Coordinates2D(100, -100), rover.getCurrentLocation());
+        rover.move("RF");
+        Assertions.assertEquals(new Coordinates2D(-100, -100), rover.getCurrentLocation());
+
+
+        rover = new MarsRoverImpl(new Coordinates2D(-100, -100), Direction.SOUTH);
+        rover.move("F");
+        Assertions.assertEquals(new Coordinates2D(-100, 100), rover.getCurrentLocation());
+        rover.move("RF");
+        Assertions.assertEquals(new Coordinates2D(100, 100), rover.getCurrentLocation());
     }
 }
